d504602f17297949d557b2e0468b797b2f057510
son <handsun000@gmail.com>
2024-12-17 10:36:45 +0900

    work

---
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -0,0 +1,35 @@
+package com.ll.tem.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+
+    @GetMapping("/write")
+    @ResponseBody
+    public String showWrite() {
+        return """
+                <form action="/posts/doWrite" method="POST">
+                    <input type="text" name="title" placeholder="제목">
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <button type="submit">글쓰기</button>
+                </form>
+                """;
+    }
+    @PostMapping("/doWrite")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+        return """
+                <h1>글쓰기 완료</h1>
+                <div>
+                    <h2>%s</h2>
+                    <p>%s</p>
+                </div>
+                """.formatted(title, content);
+    }
+}
